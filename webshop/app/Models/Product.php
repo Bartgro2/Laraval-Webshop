58944e2860dbf0bad2d5a193529db0e56ed2377c
Bart <akiraenishi@gmail.com>
2026-07-22 19:18:38 +0200

    added category and brand relationship to product model

--- webshop/app/Models/Product.php
+++ webshop/app/Models/Product.php
@@ -22,4 +22,14 @@ class Product extends Model
         'size',
         'sku',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
