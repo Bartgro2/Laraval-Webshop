019dbca43d2c8f9e0f4798e558bc9afff9734736
Bart <akiraenishi@gmail.com>
2026-08-20 14:37:28 +0200

    added the header

---
+++ webshop/resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="max-w-xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h2 class="text-2xl font-semibold text-gray-800 leading-tight text-center">
+            {{ $header }}
+        </h2>
+    </div>
+</header>
