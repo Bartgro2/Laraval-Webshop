f99e13e853170d3e816c3d7b1693e9c2ed2b95c9
Bart <akiraenishi@gmail.com>
2026-08-20 15:50:42 +0200

    added the card component for the homepage

--- webshop/resources/views/home.blade.php
+++ webshop/resources/views/home.blade.php
@@ -3,4 +3,11 @@
 
 @include('partials.navigation')
 
+<!-- Display the products in a card -->
+<!-- load the card component -->
+@foreach ($products as $product)
+    @include('components.card', ['title' => $product->name, 'content' => $product->description])
+    @include('button', ['text' => 'View Product', 'url' => route('products.show', $product->id)])
+@endforeach
+
 @include('partials.footer')
