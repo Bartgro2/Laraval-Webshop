eaeedc5a692abbc9b36a6ba237d087fcad228a7d
Bart <akiraenishi@gmail.com>
2026-08-20 14:57:37 +0200

    created an index function for the home controller with data

--- webshop/app/Http/Controllers/HomeController.php
+++ webshop/app/Http/Controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use app\Models\Product;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('home', compact('products'));
+    }
 }
