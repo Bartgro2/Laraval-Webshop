bd2946179f460e7ad47b4249c36930d3b04640e1
Bart <akiraenishi@gmail.com>
2026-09-11 09:56:48 +0200

    added: the route for the homecontroller and adjusted the home page structure

--- webshop/resources/views/home.blade.php
+++ webshop/resources/views/home.blade.php
@@ -1,13 +1,18 @@
-<!-- load the partials file -->
-@include('partials.header', ['header' => 'Welcome to the Webshop'])
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    {{ __('Welcome to the Webshop!') }}
+                </div>
+            </div>
+        </div>
+    </div>
 
-@include('partials.navigation')
-
-<!-- Display the products in a card -->
-<!-- load the card component -->
 @foreach ($products as $product)
     @include('components.card', ['title' => $product->name, 'content' => $product->description])
     @include('button', ['text' => 'View Product', 'url' => route('products.show', $product->id)])
 @endforeach
 
-@include('partials.footer')
+
+</x-app-layout>
